3d138252c1c5fc76c75855df71971a9c857a5e6b
wyy <1822364545@qq.com>
2023-02-26 15:31:56 +0800

    Fenwick-test

---
+++ DSU/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "DSU.h"
+
+using namespace scl;
+
+int main() {
+    
+    int n;
+    std::cin >> n;
+    DSU cur(n);
+
+    int a, b;
+    std::cin >> a >> b;
+    cur.merge(a, b);
+    assert(cur.same(a, b));
+
+    int x;
+    std::cin >> x;
+    std::cout << "size of " << x << " == " << cur.size(x) << '\n';
+
+    std::cout << "number of connected blocks: " << cur.count() << std::endl;
+    
+    return 0;
+}
